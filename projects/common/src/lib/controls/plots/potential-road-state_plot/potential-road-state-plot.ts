d6b0b451a612722b0abba9c894d8e5ab88b4387e
powhound4 <george.hatch@fathym.com>
2019-11-20 16:47:16 -0700

    new graphs

--- projects/common/src/lib/controls/plots/potential-road-state_plot/potential-road-state-plot.ts
+++ projects/common/src/lib/controls/plots/potential-road-state_plot/potential-road-state-plot.ts
@@ -3,23 +3,35 @@ import { Plot } from '../plots';
 export class PotentialRoadStatePlot extends Plot {
     constructor(public units: string, public subTitle: string) {
       super('Potential Road State', {
-            road_state: {
+            roadState: {
                 title: 'Potential Road State',
                 color: '#0003dd'
             }
         }, units, subTitle);
     }
-
-    getColor(val: number) {
+//  "Dry"
+// "Wet"
+// "Snow"
+// "Freezing Rain"
+// "Ice"
+// "Hail"
+// "Snow and Ice"
+// "Freezing Rain and Ice"
+// "Hail and Ice"
+    getColor(val: string) {
         val = val['Potential Road State']
 
-        if (val === null) { return '#d8d8d8'; }
-        if (val === 0) { return '#ffffff'; }
-        if (val === 1) { return '#00dd00'; }
-        if (val === 2) { return '#b760b7'; }
-        if (val === 3) { return '#0000dd'; }
-        if (val === 4) { return '#dd00dd'; }
-        if (val === 5) { return '#008888'; }
+        if (val === null) { return '#d8d8d8'; } //grey
+        if (val === "Dry") { return '#ffffff'; } //white
+        if (val === "Wet") { return '#00dd00'; }//lime
+        if (val === "Freezing Rain") { return '	#669866'; } //blue
+        if (val === "Snow") { return '#b760b7'; }//purple
+        if (val === "Snow and Ice") { return '#851A8E'; } //dark purple
+        if (val === "Freezing Rain and Ice") { return '#9F45A5'; }//light purple
+        if (val === "Ice") { return '#ff00ff'; }// dark purple
+        if (val === "Hail and Ice") { return '#80044B'; }//dark redish purple
+        if (val === "Hail") { return '#008888'; } //dark teal
+
 
         return '#ffff00';
     }
@@ -32,15 +44,21 @@ export class PotentialRoadStatePlot extends Plot {
      * Assign yAxis label (left side of chart), based off road state value
      *
      * @param val road state value
+     * 
+     * 
      */
-    getTickFormat(val: number) {
+    getTickFormat(val: string) {
 
-        if (val === 0) { return 'Clear'; }
-        if (val === 1) { return 'Wet'; }
-        if (val === 2) { return 'Mix'; }
-        if (val === 3) { return 'Frozen'; }
-        if (val === 4) { return 'Black Ice'; }
-        if (val === 5) { return 'Frost'; }
+        if (val === "Dry") { return 'Dry'; }
+        if (val === "Wet") { return 'Wet'; }
+        if (val === "Snow") { return 'Snow'; }
+        if (val === "Freezing Rain") { return 'Freezing Rain'; }
+        if (val === "Ice") { return "Ice"; }
+        if (val === "Hail") { return "Hail"; }
+        if (val === "Snow and Ice") { return "Snow and Ice"; }
+        if (val === "Freezing Rain and Ice") { return "Freezing Rain and Ice"; }
+        if (val === "Hail and Ice") { return "Hail and Ice"; }
+
 
     }
 }
