4ba1cc0a793168e1371ef6c3f97976e9ddf6dc42
powhound4 <george.hatch@fathym.com>
2019-12-09 14:57:07 -0700

    alternative routes

---
+++ projects/common/src/lib/models/route-info.model.ts
@@ -0,0 +1,17 @@
+export class RouteInfoModel{
+    public displayName: string;
+
+    public color: string;
+
+    public pointsArr: Array<atlas.data.Position>;
+
+    public avgDelayRisk: number;
+
+    public forecast: Array<any>;
+
+    public points: Array<Array<any>>;
+
+    public selectedRoute: boolean;
+
+
+}
