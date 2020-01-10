0a00b01d15955860f72416bbcf19fb3b0a25ac35
mcgear <michael.gearhardt@gmail.com>
2020-01-10 13:27:07 -0700

    Fixed lcu

--- projects/lcu/src/app/app.module.ts
+++ projects/lcu/src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { NgModule, DoBootstrap, Injector } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { CommonModule } from "@angular/common";
-import { createCustomElement } from "@angular/elements";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, DoBootstrap, Injector } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { FathymSharedModule, LCUServiceSettings } from '@lcu/common';
+import { environment } from '../environments/environment';
 import {
-  AppsManagerElementComponent,
-  SELECTOR_APPS_MANAGER_ELEMENT,
-  ContentElementComponent,
-  SELECTOR_CONTENT_ELEMENT
-} from "@lcu-ide/lcu-applications-common";
-import { LcuApplicationsModule } from "@lcu-ide/lcu-applications-common";
-import { FathymSharedModule, LCUServiceSettings } from "@lcu/common";
-import { environment } from "../environments/environment";
+  LcuFathymForecastModule,
+  LcuFathymForecastFathymForecastElementComponent,
+  SelectorLcuFathymForecastFathymForecastElement
+} from '@habistack/lcu-fathym-forecast-common';
 
 @NgModule({
   declarations: [],
@@ -19,7 +17,7 @@ import { environment } from "../environments/environment";
     BrowserModule,
     BrowserAnimationsModule,
     FathymSharedModule,
-    LcuApplicationsModule
+    LcuFathymForecastModule
   ],
   providers: [
     {
@@ -27,7 +25,7 @@ import { environment } from "../environments/environment";
       useValue: FathymSharedModule.DefaultServiceSettings(environment)
     }
   ],
-  exports: [LcuApplicationsModule]
+  exports: [LcuFathymForecastModule]
 })
 export class AppModule implements DoBootstrap {
   //  Constructors
