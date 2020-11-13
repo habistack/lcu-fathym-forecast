11aec36a75b1fd549d7fcec1d6fca68e90aa9e70
xchanin <shannonwbruns@gmail.com>
2020-11-13 09:28:23 -0700

    AB#8759 comment things out trying to figure out CORS issue

--- projects/common/src/lib/elements/api-keys/api-keys.component.ts
+++ projects/common/src/lib/elements/api-keys/api-keys.component.ts
@@ -1,10 +1,10 @@
-import { AbstractControl, FormGroup } from '@angular/forms';
+// import { AbstractControl, FormGroup } from '@angular/forms';
 import { Component, OnInit, Injector } from '@angular/core';
 import { LCUElementContext, LcuElementComponent } from '@lcu/common';
 import { FathymForecastStateContext } from '../../state/fathym-forecast/fathym-forecast-state.context';
 import { FathymForecastState } from '../../state/fathym-forecast/fathym-forecast.state';
-import { FormControl } from '@angular/forms';
-import { Validators } from '@angular/forms';
+// import { FormControl } from '@angular/forms';
+// import { Validators } from '@angular/forms';
 
 export class LcuFathymForecastApiKeysElementState {}
 
@@ -26,41 +26,41 @@ export class LcuFathymForecastApiKeysElementComponent
   implements OnInit {
   //  Fields
 
-/**
- * Access primaryKeyControl field within the form group
- */
-public get PrimaryKeyControl(): AbstractControl {
-  return this.KeysFormGroup.get('primaryKeyControl');
-}
+// /**
+//  * Access primaryKeyControl field within the form group
+//  */
+// public get PrimaryKeyControl(): AbstractControl {
+//   return this.KeysFormGroup.get('primaryKeyControl');
+// }
 
-/**
- * Access secondaryKeyControl field within the form group
- */
-public get SecondaryKeyControl(): AbstractControl {
-  return this.KeysFormGroup.get('secondaryKeyControl');
-}
+// /**
+//  * Access secondaryKeyControl field within the form group
+//  */
+// public get SecondaryKeyControl(): AbstractControl {
+//   return this.KeysFormGroup.get('secondaryKeyControl');
+// }
 
   //  Properties
   public APIKeyTypes: string[];
 
-  /**
-   * Toggle visibility of primary key
-   */
-  public HidePrimaryKey: boolean;
+  // /**
+  //  * Toggle visibility of primary key
+  //  */
+  // public HidePrimaryKey: boolean;
 
-  /**
-   * Toggle visibility of secondary key
-   */
-  public HideSecondaryKey: boolean;
+  // /**
+  //  * Toggle visibility of secondary key
+  //  */
+  // public HideSecondaryKey: boolean;
 
-  /**
-   * Form group for primary and secondary keys
-   */
-  public KeysFormGroup: FormGroup;
+  // /**
+  //  * Form group for primary and secondary keys
+  //  */
+  // public KeysFormGroup: FormGroup;
 
-  public LinkRegen: string;
+  // public LinkRegen: string;
 
-  public LinkDocumentation: string;
+  // public LinkDocumentation: string;
 
   /**
    * Forecast state
@@ -89,22 +89,22 @@ public get SecondaryKeyControl(): AbstractControl {
   ) {
     super(injector);
 
-    this.Title = 'Fathym Forecaster';
-    this.PageDescription = 'Harnessing the power of machine learning to create safer roads.'
-    this.PageDescriptionDetails = 'The Fathym Forecaster combines the world\'s best weather \
-                                   forecasts with statistics-based, machine-learning techniques \
-                                   to tackle the largest datasets. including road weather.';
-    this.LinkRegen = 'https://lcu-prd.portal.azure-api.net/developer#';
-    this.LinkDocumentation = 'https://support.fathym.com/docs/fathym-forecast-api';
+    // this.Title = 'Fathym Forecaster';
+    // this.PageDescription = 'Harnessing the power of machine learning to create safer roads.'
+    // this.PageDescriptionDetails = 'The Fathym Forecaster combines the world\'s best weather \
+    //                                forecasts with statistics-based, machine-learning techniques \
+    //                                to tackle the largest datasets. including road weather.';
+    // this.LinkRegen = 'https://lcu-prd.portal.azure-api.net/developer#';
+    // this.LinkDocumentation = 'https://support.fathym.com/docs/fathym-forecast-api';
 
-    this.setFieldToggles();
+    // this.setFieldToggles();
   }
 
   //  Life Cycle
   public ngOnInit() {
     super.ngOnInit();
 
-    this.setupForm();
+    // this.setupForm();
 
     this.forecastCtxt.Context.subscribe((state) => {
       this.State = state;
@@ -117,51 +117,51 @@ public get SecondaryKeyControl(): AbstractControl {
 
   //  API Methods
 
-  public ExternalLink(val: string): void {
-    window.open(val, '_blank');
-  }
+  // public ExternalLink(val: string): void {
+  //   window.open(val, '_blank');
+  // }
 
   public Description(): void {
 
   }
 
   //  Helpers
-  protected setupForm(): void {
-    this.KeysFormGroup = new FormGroup({
-      primaryKeyControl: new FormControl('', {validators: Validators.required}),
-      secondaryKeyControl: new FormControl('', {validators: Validators.required})
-    });
-  }
+  // protected setupForm(): void {
+  //   this.KeysFormGroup = new FormGroup({
+  //     primaryKeyControl: new FormControl('', {validators: Validators.required}),
+  //     secondaryKeyControl: new FormControl('', {validators: Validators.required})
+  //   });
+  // }
 
   /**
    * Setup Initila toggle values for inputs
    */
-  protected setFieldToggles(): void {
-    this.HidePrimaryKey = this.HideSecondaryKey = true;
-  }
+  // protected setFieldToggles(): void {
+  //   this.HidePrimaryKey = this.HideSecondaryKey = true;
+  // }
 
   /**
    * Listen for state changes
    */
   protected stateChanged() {
-    if (this.State.APIKeys) {
-      this.APIKeyTypes = Object.keys(this.State.APIKeys);
-      this.updateKeyValues();
-    } else {
-      this.APIKeyTypes = [];
-    }
+    // if (this.State.APIKeys) {
+    //   this.APIKeyTypes = Object.keys(this.State.APIKeys);
+    //   this.updateKeyValues();
+    // } else {
+    //   this.APIKeyTypes = [];
+    // }
   }
 
   /**
    * Update API Key values
    */
   protected updateKeyValues(): void {
-    this.PrimaryKeyControl.setValue(
-      this.State.APIKeys.primaryKey || ''
-    );
+    // this.PrimaryKeyControl.setValue(
+    //   this.State.APIKeys.primaryKey || ''
+    // );
 
-    this.SecondaryKeyControl.setValue(
-      this.State.APIKeys.secondaryKey || ''
-    );
+    // this.SecondaryKeyControl.setValue(
+    //   this.State.APIKeys.secondaryKey || ''
+    // );
   }
 }
--- projects/common/src/lib/elements/routing/routing.component.ts
+++ projects/common/src/lib/elements/routing/routing.component.ts
@@ -1,10 +1,9 @@
-import { FathymForecastState } from './../../state/fathym-forecast/fathym-forecast.state';
+// import { FathymForecastState } from './../../state/fathym-forecast/fathym-forecast.state';
 import { Component, OnInit, Injector, OnDestroy } from '@angular/core';
-import { LCUElementContext, LcuElementComponent, RouterHelpersService } from '@lcu/common';
-import { FathymForecastStateContext } from '../../state/fathym-forecast/fathym-forecast-state.context';
-import { ActivatedRoute } from '@angular/router';
-import { combineLatest } from 'rxjs/internal/operators/combineLatest';
-import { Subscription } from 'rxjs/internal/Subscription';
+import { LCUElementContext, LcuElementComponent } from '@lcu/common';
+// import { FathymForecastStateContext } from '../../state/fathym-forecast/fathym-forecast-state.context';
+// import { ActivatedRoute } from '@angular/router';
+// import { Subscription } from 'rxjs/internal/Subscription';
 // import { filter } from 'rxjs/operators';
 
 export class LcuFathymForecastRoutingElementState {}
@@ -26,25 +25,22 @@ export class LcuFathymForecastRoutingElementComponent extends
 
   public ShowPage: string;
 
-  public State: FathymForecastState;
+  // public State: FathymForecastState;
 
-  protected routeSubscription: Subscription;
+  // protected routeSubscription: Subscription;
 
 
 
   //  Constructors
   constructor(
-    protected injector: Injector,
-    protected forecastCtxt: FathymForecastStateContext,
-    protected routerHelperService: RouterHelpersService,
-    protected route: ActivatedRoute) {
+    protected injector: Injector) {
     super(injector);
 
     this.ShowPage = '';
   }
 
   ngOnDestroy(): void {
-    this.routeSubscription.unsubscribe();
+    // this.routeSubscription.unsubscribe();
     throw new Error('Method not implemented.');
   }
 
@@ -52,17 +48,17 @@ export class LcuFathymForecastRoutingElementComponent extends
   public ngOnInit() {
     super.ngOnInit();
 
-    this.routeSubscription = this.route.queryParams.subscribe((params: any) => {
-     this.queryParamCallback(params);
-    });
+    // this.routeSubscription = this.route.queryParams.subscribe((params: any) => {
+    //  this.queryParamCallback(params);
+    // });
 
-    this.forecastCtxt.Context.subscribe((state) => {
-      this.State = state;
+    // this.forecastCtxt.Context.subscribe((state) => {
+    //   this.State = state;
 
-      if (this.State) {
-        this.stateChanged();
-      }
-    });
+    //   if (this.State) {
+    //     this.stateChanged();
+    //   }
+    // });
   }
 
   //  API Methods
@@ -77,17 +73,17 @@ export class LcuFathymForecastRoutingElementComponent extends
    *
    * @param val object returned from route subscription
    */
-  protected queryParamCallback(val: object): void {
-    const length: number = Object.keys(val).length;
+  // protected queryParamCallback(val: object): void {
+  //   const length: number = Object.keys(val).length;
 
-    if (length > 0) {
-      for (const key in val) {
-        if (key.toUpperCase() === 'ROUTE') {
-          this.ShowPage = val[key].toUpperCase();
-        }
-      }
-    } else {
-      this.ShowPage = 'API';
-    }
-  }
+  //   if (length > 0) {
+  //     for (const key in val) {
+  //       if (key.toUpperCase() === 'ROUTE') {
+  //         this.ShowPage = val[key].toUpperCase();
+  //       }
+  //     }
+  //   } else {
+  //     this.ShowPage = 'API';
+  //   }
+  // }
 }
