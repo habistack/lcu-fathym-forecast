4e99fa224884e10f5f1fb5dfc8c618b89edec4b3
xchanin <shannonwbruns@gmail.com>
2020-11-13 09:04:51 -0700

    AB#8759 comment out router components

--- projects/demo/src/app/app-routing.module.ts
+++ projects/demo/src/app/app-routing.module.ts
@@ -1,21 +1,19 @@
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {
-         LcuFathymForecastFathymForecastElementComponent,
-         LcuFathymForecastRoutingElementComponent } from '@habistack/lcu-fathym-forecast-common';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: 'page-route/api', pathMatch: 'full'},
-  { path: 'page-route/api',
-    component: LcuFathymForecastRoutingElementComponent
-  },
-  { path: 'page-route/:route',
-    component: LcuFathymForecastRoutingElementComponent
-  },
-  { path: 'route-search',
-    component: LcuFathymForecastFathymForecastElementComponent
-  }
+  // { path: '', redirectTo: 'page-route/api', pathMatch: 'full'},
+  // { path: 'page-route/api',
+  //   component: LcuFathymForecastRoutingElementComponent
+  // },
+  // { path: 'page-route/:route',
+  //   component: LcuFathymForecastRoutingElementComponent
+  // },
+  // { path: 'route-search',
+  //   component: LcuFathymForecastFathymForecastElementComponent
+  // }
 ];
 
 @NgModule({
