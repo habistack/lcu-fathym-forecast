8f70103926381b428f109ed882acf27ed9981448
xchanin <shannonwbruns@gmail.com>
2020-11-14 08:37:28 -0700

    AB#8759 update routing module

--- projects/demo/src/app/app-routing.module.ts
+++ projects/demo/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {
+         LcuFathymForecastFathymForecastElementComponent,
+         LcuFathymForecastRoutingElementComponent, 
+         RouterGuard} from '@habistack/lcu-fathym-forecast-common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'page-route', pathMatch: 'full'
+  },
+  {
+    path: 'page-route',
+    component: LcuFathymForecastRoutingElementComponent
+  },
+  {
+    path: 'page-route/:route',
+    component: LcuFathymForecastRoutingElementComponent,
+    canActivate: [RouterGuard]
+  },
+  {
+    path: 'route-search',
+    component: LcuFathymForecastFathymForecastElementComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
