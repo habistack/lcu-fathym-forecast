3c0d4566d6a4a4d23fd84878d66621bb24788aca
xchanin <shannonwbruns@gmail.com>
2020-11-16 06:29:27 -0700

    AB#8759 remove statecontext

--- projects/common/src/lib/elements/routing/routing.component.ts
+++ projects/common/src/lib/elements/routing/routing.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Injector } from '@angular/core';
-import { Subscription } from 'rxjs/internal/Subscription';
-import { LCUElementContext, LcuElementComponent } from '@lcu/common';
+import { Component, OnInit, Injector, OnDestroy } from '@angular/core';
+import { LCUElementContext, LcuElementComponent, RouterHelpersService } from '@lcu/common';
 import { FathymForecastState } from './../../state/fathym-forecast/fathym-forecast.state';
-import { FathymForecastStateContext } from '../../state/fathym-forecast/fathym-forecast-state.context';
+// import { FathymForecastStateContext } from '../../state/fathym-forecast/fathym-forecast-state.context';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/internal/Subscription';
+// import { filter } from 'rxjs/operators';
 
 export class LcuFathymForecastRoutingElementState {}
 
@@ -16,30 +17,75 @@ export const SELECTOR_LCU_FATHYM_FORECAST_ROUTING_ELEMENT = 'lcu-fathym-forecast
   templateUrl: './routing.component.html',
   styleUrls: ['./routing.component.scss']
 })
-export class LcuFathymForecastRoutingElementComponent extends 
-                LcuElementComponent<LcuFathymForecastRoutingContext> implements OnInit {
+export class LcuFathymForecastRoutingElementComponent extends
+  LcuElementComponent<LcuFathymForecastRoutingContext> implements OnInit, OnDestroy {
   //  Fields
 
   //  Properties
-  public ShowPage: string = 'API';
+
+  public ShowPage: string;
 
   public State: FathymForecastState;
 
   protected routeSubscription: Subscription;
 
+
+
   //  Constructors
-  constructor(protected injector: Injector,
+  constructor(
+    protected injector: Injector,
+    protected routerHelperService: RouterHelpersService,
     protected route: ActivatedRoute) {
     super(injector);
+
+    this.ShowPage = '';
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
+    throw new Error('Method not implemented.');
   }
 
   //  Life Cycle
   public ngOnInit() {
     super.ngOnInit();
+
+    this.routeSubscription = this.route.queryParams.subscribe((params: any) => {
+     this.queryParamCallback(params);
+    });
+
+    // this.forecastCtxt.Context.subscribe((state) => {
+    //   this.State = state;
+
+    //   if (this.State) {
+    //     this.stateChanged();
+    //   }
+    // });
   }
 
   //  API Methods
 
   //  Helpers
-}
+  protected stateChanged() {
 
+  }
+
+  /**
+   * Callback for route subscription
+   *
+   * @param val object returned from route subscription
+   */
+  protected queryParamCallback(val: object): void {
+    const length: number = Object.keys(val).length;
+    debugger;
+    if (length > 0) {
+      for (const key in val) {
+        if (key.toUpperCase() === 'ROUTE') {
+          this.ShowPage = val[key].toUpperCase();
+        }
+      }
+    } else {
+      this.ShowPage = 'API';
+    }
+  }
+}
