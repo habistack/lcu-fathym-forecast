2acc4fec7d6db5a459e14576640562a6e75a7778
xchanin <shannonwbruns@gmail.com>
2020-11-16 05:00:08 -0700

    AB#8759 slowly add in stuff until it breaks

--- projects/common/src/lib/elements/routing/routing.component.ts
+++ projects/common/src/lib/elements/routing/routing.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Injector } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
 import { LCUElementContext, LcuElementComponent } from '@lcu/common';
+import { FathymForecastState } from './../../state/fathym-forecast/fathym-forecast.state';
+import { FathymForecastStateContext } from '../../state/fathym-forecast/fathym-forecast-state.context';
 
 export class LcuFathymForecastRoutingElementState {}
 
@@ -12,11 +15,17 @@ export const SELECTOR_LCU_FATHYM_FORECAST_ROUTING_ELEMENT = 'lcu-fathym-forecast
   templateUrl: './routing.component.html',
   styleUrls: ['./routing.component.scss']
 })
-export class LcuFathymForecastRoutingElementComponent extends LcuElementComponent<LcuFathymForecastRoutingContext> implements OnInit {
+export class LcuFathymForecastRoutingElementComponent extends 
+                LcuElementComponent<LcuFathymForecastRoutingContext> implements OnInit {
   //  Fields
 
   //  Properties
-public ShowPage: string = 'API';
+  public ShowPage: string = 'API';
+
+  public State: FathymForecastState;
+
+  protected routeSubscription: Subscription;
+
   //  Constructors
   constructor(protected injector: Injector) {
     super(injector);
@@ -31,3 +40,4 @@ public ShowPage: string = 'API';
 
   //  Helpers
 }
+
