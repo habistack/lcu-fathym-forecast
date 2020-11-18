41fc4070801d3131098aeefa356f6517f4f36ad7
xchanin <shannonwbruns@gmail.com>
2020-11-18 09:58:20 -0700

    AB#8759 add FathymForecastStateContext to no-access component for testing

--- projects/common/src/lib/elements/no-access/no-access.component.ts
+++ projects/common/src/lib/elements/no-access/no-access.component.ts
@@ -11,7 +11,7 @@ export const SELECTOR_LCU_FATHYM_FORECAST_NO_ACCESS_ELEMENT =
 })
 export class LcuFathymForecastNoAccessComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected forecastCtxt: FathymForecastStateContext) { }
 
   ngOnInit(): void {
   }
