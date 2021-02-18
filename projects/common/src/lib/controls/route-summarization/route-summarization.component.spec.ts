4c751ee5f3494649d459957fc3fc3f573ffb54af
Jack Penhale <jack.penhale@fathym.com>
2021-02-18 16:15:05 -0700

    cli+core

--- projects/common/src/lib/controls/route-summarization/route-summarization.component.spec.ts
+++ projects/common/src/lib/controls/route-summarization/route-summarization.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RouteSummarizationComponent } from './route-summarization.component';
 
@@ -6,7 +6,7 @@ describe('RouteSummarizationComponent', () => {
   let component: RouteSummarizationComponent;
   let fixture: ComponentFixture<RouteSummarizationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RouteSummarizationComponent ]
     })
